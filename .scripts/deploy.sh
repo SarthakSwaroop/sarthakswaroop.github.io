7e440772076c44719a76be3ac0bf7e66dc5d3c8e
Sarthak Swaroop Bansal <52138161+SarthakSwaroop@users.noreply.github.com>
2019-10-14 23:48:14 +0530

    Create deploy.sh

---
+++ .scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+if git diff --name-only --cached
+then
+	git config --global user.name "Travis CI"
+	git config --global user.email "travis@travis-ci.com"
+
+	git fetch --unshallow || true
+	git checkout master
+	git add projects _projects _people _orgs
+	git commit -m "Auto-commit (Travis) $TRAVIS_BUILD_NUMBER [ci skip]"
+	git remote set-url origin https://fennifith:${GITHUB_TOKEN}@github.com/fennifith/fennifith.github.io.git/
+	git push origin master
+fi
+
+exit 0
