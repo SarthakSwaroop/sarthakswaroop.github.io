a9427c96d3d97c043ba0569b15ce117af715061d
Sarthak Swaroop Bansal <52138161+SarthakSwaroop@users.noreply.github.com>
2019-10-19 23:55:43 +0530

    Update deploy.sh

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -10,7 +10,7 @@ then
 	git checkout master
 	git add projects _projects _people _orgs
 	git commit -m "Auto-commit (Travis) $TRAVIS_BUILD_NUMBER [ci skip]"
-	git remote set-url origin https://fennifith:${GITHUB_TOKEN}@github.com/fennifith/fennifith.github.io.git/
+	git remote set-url origin https://sarthak:${GITHUB_TOKEN}@github.com/sarthakswaroop/sarthakswaroop.github.io.git/
 	git push origin master
 fi
 
